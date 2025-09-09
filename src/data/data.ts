485613bee6e26ee80638e87a132abd15707beebc
Yaroslav <bogdan.az62@gmail.com>
2025-09-09 22:57:04 +0300

    last commit2

--- src/data/data.ts
+++
@@ -1,8 +0,0 @@
-import type {TTodo} from "../types/todo.ts";
-
-export const todoDate: TTodo[] = [
-    { id: 1, title: 'Buy a milk', description: 'Varus', status: 'todo', createdAt: '12121' },
-    { id: 2, title: 'Take a Note', description: 'ToDo', status: 'todo', createdAt: '13131' },
-    { id: 3, title: 'Learn TypeScript + React', description: 'Google', status: 'todo', createdAt: '14141' },
-    { id: 4, title: 'Have a call with Kostya', description: 'GMeet', status: 'todo', createdAt: '15151' }
-]
